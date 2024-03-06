728e7a5c5c78b1c2f854b5b1f91d790bc8d21e9f
looechao <lu.chao.personal@foxmail.com>
2024-03-06 16:12:15 +0800

    题目：欢乐的跳-修改完成-All AC

--- p1152.cpp
+++ p1152.cpp
@@ -11,18 +11,22 @@ int main()
     int n;
     int judge=0;
     cin>>n;
-    int flag[n];
-    for(int i=0;i<n;i++){   //存放数组内元素的标记
-        flag[i]=0;
-    }
-    int num[n];
-    for(int i=0;i<n;i++)  //输入数组
+    vector<int> flag(n,0);   //初始化flag和num动态数组
+    vector<int> num(n,0);
+
+    for(int i=0;i<n;i++)  //输入num数组
     {
         cin>>num[i];
     }
     for(int i=0;i<n-1;i++)    //判断差值是否在数组内
     {
         int diff=abs(num[i+1]-num[i]);
+        //增加一个防止越界访问的判读
+        if(diff>n)
+        {
+            cout<<"Not jolly";
+            return 0;
+        }
         flag[diff]=1;   //在数组内，标记为1
     }
     for(int i=1;i<n;i++)
