eeab68c74a0052204b348a6aedbcc4ca14221d73
looechao <lu.chao.personal@foxmail.com>
2024-03-11 21:16:45 +0800

    题目-西游记公司-通过不了测试-需要后续更正

---
+++ p1614.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<vector>
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+#include<limits>
+using namespace std;
+
+int main()
+{
+    int n=0,m=0;
+    cin>>n>>m;
+    int count=0;  //记数统计
+    int p=0;   //指针|游标
+    int min=numeric_limits<int>::max();
+    vector<int> num(n,0);
+    for(int i=0;i<n;i++)
+    {
+        cin>>num[i];
+    }
+    while(p<=n-m)
+    {
+        for(int i=p;i<p+m;i++)
+        {
+            count+=num[i];
+        }
+        if(count<min) min=count;
+        count = 0;
+        p++;
+    }
+    cout<<min;
+    return 0;
+}
