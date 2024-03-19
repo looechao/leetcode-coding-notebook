a64ad39df19234299691944543ed89e4bb883d10
looechao <lu.chao.personal@foxmail.com>
2024-03-19 10:34:14 +0800

    题目-金币-最终版

--- p2669.cpp
+++ p2669.cpp
@@ -6,34 +6,20 @@
 #include<cmath>
 using namespace std;
 
+int k;
+
 int main()
 {
-    long long k;
-    cin >> k;
-
-    long long sum = 0; // 记录总和
-    long long n = 0;   // 当前正整数
-    long long total = 0; // 总天数
-
-    // 用余额法代替内层循环
-    while (total < k)
+    long long sum=0;
+    long long n=1;
+    long long days=0;
+    cin>>k;
+    for(int i=1;i<=k;i++)
     {
-        ++n;
-        total += n;
+        sum+=n;
+        days++;
+        if(days==n) {n++;days=0;}
     }
-
-    // 如果总天数超过k，差值（total - k）就是多余的天数，需要从总和中扣除
-    if (total > k)
-    {
-        sum = (1 + n) * n / 2;
-        sum -= total - k;
-    }
-    else
-    {
-        // 否则就是相等直接使用公式计算
-        sum = (1 + n) * n / 2;
-    }
-
-    cout << sum << endl;
+    cout<<sum;
     return 0;
 }
