f9c4c2b659c0dca70c47cba64a47d39dc6853699
looechao <lu.chao.personal@foxmail.com>
2024-03-21 10:33:46 +0800

    题目-r2-提交完毕

---
+++ p4413.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<stdio.h>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int s,r1,r2;
+
+int main()
+{
+    cin>>r1>>s;
+    cout<<2*s-r1;
+    return 0;
+}
