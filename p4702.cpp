b9aa947a0e89cb39bc4d5c10ede7f768db14f96d
looechao <lu.chao.personal@foxmail.com>
2024-03-21 15:45:02 +0800

    题目-取石子-提交完成-算位优化方法

--- p4702.cpp
+++ p4702.cpp
@@ -5,30 +5,19 @@
 #include<vector>
 using namespace std;
 
-int n;
+long long sum,n,a,b;
 
 int main()
 {
     cin>>n;
     bool alice=true;
-    vector<int> num(n+1,0);
-    for(int i=1;i<=n;i++)
+    vector<long long> num(n+1,0);
+    for(long long i=1;i<=n;i++)
     {
         cin>>num[i];
+        sum+=num[i];
     }
-    for(int i=1;;i++)
-    {
-        sort(num.begin(),num.end());
-        if(alice) //Alice取石子
-        {
-            if(num[n]==0) {cout<<"Bob"; return 0;}
-            else {num[n]--; alice=!alice;}
-        }
-        if(!alice) //Bob取石子
-        {
-            if(num[n]==0) {cout<<"Alice"; return 0;}
-            else {num[n]--; alice=!alice;}
-        }
-    }
+    if(!(sum&1)) cout<<"Bob";
+    else cout<<"Alice";
     return 0;
 }
