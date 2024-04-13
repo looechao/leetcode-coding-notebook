523b16361528694bd901df0109d956537cb56013
looechao <lu.chao.personal@foxmail.com>
2024-04-13 15:16:57 +0800

    优化时间复杂度

--- coding/p0134.cpp
+++ coding/p0134.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int left=0,spot=0;
-        for(int i=0;i<gas.size();i++){     //从不同的起点出发
-            int j=i;
-            left=0;
-            while(1){
-                left+=gas[j];    //加油
-                if(left<cost[j]) // 当前的汽油不足以抵达下一加油站
-                    break;
-                left-=cost[j];   //耗油
-                j=(j+1)%gas.size();   //前进到下一个加油站，如果到达末尾则回到开始
-                if(j==i) // 如果我们成功绕环一圈，返回开始的索引
-                    return i;
+        int total_gas = 0, total_cost = 0, tank = 0, start = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            total_gas += gas[i];   // 所有站点的油量
+            total_cost += cost[i]; // 所有站点的耗油量
+            tank += gas[i] - cost[i]; // tank 存储的是剩余的油量
+
+            if (tank < 0) { // 如果油量不够
+                start = i + 1; // 选择下一个站点作为新的出发点
+                tank = 0;      // 油箱置空
             }
         }
-        return -1;
+
+        // 如果总油量 >= 总消耗，那么一定可以找到一个起始点走完整个路程
+        if (total_gas >= total_cost) return start;
+        else return -1;
     }
 };
